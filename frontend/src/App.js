8f2ca1ff1ee649d76ffb2c67425520736a72020d
Karen Lara <karen.lara1607@gmail.com>
2021-06-16 00:02:54 -0300

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      Bem vindo
+    </div>
+  );
+}
+
+// adc yarn add http-proxy-middleware
+// adc npm install concurrently --save-dev
+
+export default App;
